7e3a665545e9e7af9bbb6b07fe0c1377d4d84f70
Alex <coolazn818@hotmail.com>
2020-09-13 17:06:35 +0930

    changed leaderboard view

--- lib/views/leaderboard.php
+++ lib/views/leaderboard.php
@@ -9,8 +9,15 @@ tr:nth-child(3) {
 tr:nth-child(4) {
   background-color: brown;
 }
+
+.rank, .points {
+  width: 40px;
+}
 </style>
 
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
 
 <?php
 echo "<h1>Leader Board</h1>";
@@ -18,9 +25,9 @@ echo "<h1>Leader Board</h1>";
 
  echo "<table>
    <tr>
-     <th>Rank</th>
+     <th class = 'rank'>Rank</th>
      <th>Name</th>
-     <th>Points</th>
+     <th class = 'points'>Points</th>
    </tr>
 ";
   //Print the list of account details
@@ -42,9 +49,13 @@ echo "<h1>Leader Board</h1>";
 
    echo "
      <tr>
-       <td>{$rank}</td>
+       <td>{$rank}
+          <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-trophy-fill' fill='black' xmlns='http://www.w3.org/2000/svg'>
+            <path fill-rule='evenodd' d='M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z'/>
+          </svg>
+       </td>
        <td>{$fname}</td>
-       <td>{$points}</td>
+       <td>{$points} pts</td>
      </tr>
    "
    ;
